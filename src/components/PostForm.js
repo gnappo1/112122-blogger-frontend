707d769893a618b5ad06105cba301edcd1683f7b
Matteo Piccini <gnappo.1@live.it>
2023-02-03 15:24:50 -0800

    Change form to handle errors 'in-house'

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,7 +1,8 @@
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 import {useHistory} from "react-router-dom"
 const PostForm = ({authors, setPosts, setError}) => {
     const history = useHistory()
+    const [formErrors, setFormErrors] = useState({})
 
     const [newPost, setNewPost] = useState({
         summary: "",
@@ -30,18 +31,18 @@ const PostForm = ({authors, setPosts, setError}) => {
             if (res.status === 201) {
                 res.json().then(postFromBackend => {
                     setPosts(currentPosts => [postFromBackend, ...currentPosts])
+                    setError({text: "Post successfully created", type: "success"})
+                    history.push("/")
                 })
-                setError({text: "Post successfully created", type: "success"})
-                history.push("/")
             } else {
                 res.json().then(messageObj => {
                     if (typeof(messageObj.error) === 'string') {
                         setError({text: messageObj.error, type: "error"})
                     } else {
 
-                        const oneStringErrors = Object.entries(messageObj.error).map(errArray => `${errArray[0]}: ${errArray[1]}`).join(". ")
+                        // const oneStringErrors = Object.entries(messageObj.error).map(errArray => `${errArray[0]}: ${errArray[1]}`).join(". ")
                         // debugger
-                        setError({text: oneStringErrors, type: "error"})
+                        setFormErrors(messageObj.error)
                     }
                 })
             }
@@ -60,22 +61,27 @@ const PostForm = ({authors, setPosts, setError}) => {
     <div>
         <form onSubmit={handleSubmit}>
             <label htmlFor='title'>Title</label>
-            <input type="text" name="title" value={newPost.title} onChange={handleChange}/>
+            <input type="text" name="title" value={newPost.title} onChange={handleChange}/><br />
+            {formErrors.title ? <><span className='error'>{formErrors.title.join(". ")}</span><br /></> : null}
             <label htmlFor='author'>Author</label>
             <select name="author_id" onChange={handleChange} value={newPost.author_id}>
                 <option value="">Select an Author</option>
                 {authors.map(author => <option key={author.id} value={author.id}>{author.name}</option>)}
-            </select>
+            </select><br />
+            {formErrors.author_id ? <><span className='error'>{formErrors.author_id.join(". ")}</span><br /></> : null}
             <label htmlFor='category'>Category</label>
             <select name="category" onChange={handleChange} value={newPost.category}>
                 <option value="">Select a Category</option>
                 <option value="Fiction">Fiction</option>
                 <option value="Non-Fiction">Non Fiction</option>
-            </select>
+            </select><br />
+            {formErrors.category ? <><span className='error'>{formErrors.category.join(". ")}</span><br /></> : null}
             <label htmlFor='content'>Content</label>
-            <input type="text" name="content" value={newPost.content} onChange={handleChange}/>
+            <input type="text" name="content" value={newPost.content} onChange={handleChange}/><br />
+            {formErrors.content ? <><span className='error'>{formErrors.content.join(". ")}</span><br /></> : null}
             <label htmlFor='summary'>Summary</label>
-            <input type="text" name="summary" value={newPost.summary} onChange={handleChange}/>
+            <input type="text" name="summary" value={newPost.summary} onChange={handleChange}/><br />
+            {formErrors.summary ? <><span className='error'>{formErrors.summary.join(". ")}</span><br /></> : null}
             <input type="submit" value="create"/>
         </form>
     </div>
